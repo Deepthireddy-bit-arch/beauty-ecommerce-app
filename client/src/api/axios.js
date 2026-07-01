441bbed0d5e7722f86c3d3aa546f0d6494811d7a
Deepthi Reddy <deepthi.d@aimwindow.co.in>
2026-07-01 15:08:03 +0530

    added base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,57 +1,7 @@
-// // import axios from "axios";
 
-// // const api = axios.create({
-// //   baseURL: "/api",
-// // });
-
-// // // auto-attach token to every request
-// // api.interceptors.request.use((config) => {
-// //   const token = localStorage.getItem("token");
-// //   if (token) config.headers.Authorization = `Bearer ${token}`;
-// //   return config;
-// // });
-
-// // export default api;
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "/api",
-// });
-
-// // auto-attach token to every request
-// api.interceptors.request.use((config) => {
-//   // Use "token" since that's what's in localStorage
-//   const token = localStorage.getItem("token"); // Changed from "shophub_token" to "token"
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// // Response interceptor - handle 401
-// api.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       console.log("🔴 401 Unauthorized - clearing token");
-//       localStorage.removeItem("token");
-//       localStorage.removeItem("user"); // or whatever key you use for user
-      
-//       if (window.location.pathname !== "/login") {
-//         window.location.href = "/login";
-//       }
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
 import axios from "axios";
 
-// Use environment variable or default
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`;
 
 const api = axios.create({
   baseURL: API_URL,
